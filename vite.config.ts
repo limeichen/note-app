df7e9bffd86960af77b0d07e227461280be49109
limeichen <2482698881@qq.com>
2024-06-20 23:57:44 +0800

    测试，修改vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VUE_APP_PUBLIC_PATH || '/',
-  // base: process.env.NODE_ENV === 'production' ? './' : '/',
-
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: process.env.VUE_APP_PUBLIC_PATH || '/',
+    // base: process.env.NODE_ENV === 'production' ? './' : '/',
+    plugins: [
+        vue(),
+        vueJsx(),
+        VueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
