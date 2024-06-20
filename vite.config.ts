147a824c613e4ee282a0f126cf6e4b89e7d248a1
limeichen <2482698881@qq.com>
2024-06-20 23:31:16 +0800

    测试，修改vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: process.env.VUE_APP_PUBLIC_PATH || '/',
-  base: process.env.NODE_ENV === 'production' ? './' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/aaaaaa/' : '/',
   plugins: [
     vue(),
     vueJsx(),
